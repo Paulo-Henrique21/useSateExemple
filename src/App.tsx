66a5e230f48ad7e354e71899d12a24d64244f574
Paulo Henrique de Santana de Souza <psantana@safira.local>
2022-12-23 01:01:09 -0300

    Foi criado esse projeto com o principio de mostrar algumas funcionalidades do useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { Footer } from "./components/Footer";
+import { Header } from "./components/Header";
 
-function App() {
+export default function App (){
+
+  const [name, setName] = useState("Bonieky") 
+  const [age, setAge] = useState(90)
+  const [bg, setBg] = useState('#ff0000')
+
+  const[list, setList] = useState([
+    "O rato Roeu a roupa do rei de Roma.",
+    "Aqui vai uma frase poética.",
+    "A inspiração acabou."
+  ])
+
+  const handle20 = () => {
+    setAge(20)
+    setBg("#0000ff")
+  }
+
+  const handle90 = () => {
+    setAge(90)
+    setBg("#ff0000")
+  }
+
+  const handleRed = () => {
+    setBg("#ff0000")
+  }
+
+  const handleBlue = () => {
+    setBg("#0000ff")
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div style={{backgroundColor: bg}}>
+      <Header name={name} age={age} />
 
-export default App;
+      {age === 90 && 
+      <button onClick={handle20}>Tenho 20 anos</button>
+      }
+
+      {age === 20 && 
+      <button onClick={handle90}>Tenho 90 anos</button>
+      }
+      
+      {bg === "#ff0000" &&
+      <button onClick={handleBlue}>Azul</button>
+      }
+
+      {bg === "#0000ff" && 
+      <button onClick={handleRed}>vermelho</button>
+      }
+
+      <hr/>
+
+      <ul>
+        {list.map((frase, index)=>(
+          <li key={index}>{frase}</li>
+        ))}
+      </ul>
+
+      <Footer/>
+    </div>
+  )
+}
